b1743e0df5933ff1f9104a98119612e9bd9a2afd
715314 <715314@PCIN472918.cts.com>
2019-11-28 13:54:42 +0530

    Springboot-vaadin basic app

--- Vaadindemo/src/main/java/springboot/spring/Application.java
+++ Vaadindemo/src/main/java/springboot/spring/Application.java
@@ -1,20 +1,13 @@
 package springboot.spring;
 
-import java.util.Optional;
-
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.client.RestTemplate;
 
-import springboot.spring.entity.DepartmentEntity;
-import springboot.spring.entity.EmployeeEntity;
-import springboot.spring.repository.DepartmentRepository;
-import springboot.spring.repository.EmployeeRepository;
-
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class Application {
 
 	public static void main(String[] args) {
@@ -23,151 +16,9 @@ public class Application {
 	}
 	
 	@Bean
-	public DepartmentRepository Departmentrep()
+	public RestTemplate getRestTemplate()
 	{
-		return new DepartmentRepository() {
-			
-			@Override
-			public <S extends DepartmentEntity> Iterable<S> saveAll(Iterable<S> arg0) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public <S extends DepartmentEntity> S save(S arg0) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Optional<DepartmentEntity> findById(Integer arg0) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Iterable<DepartmentEntity> findAllById(Iterable<Integer> arg0) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Iterable<DepartmentEntity> findAll() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public boolean existsById(Integer arg0) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-			
-			@Override
-			public void deleteById(Integer arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void deleteAll(Iterable<? extends DepartmentEntity> arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void deleteAll() {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void delete(DepartmentEntity arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public long count() {
-				// TODO Auto-generated method stub
-				return 0;
-			}
-		};
-		
-		
-	}
-	
-	@Bean
-	public EmployeeRepository employeerep()
-	{
-		return new EmployeeRepository() {
-			
-			@Override
-			public <S extends EmployeeEntity> Iterable<S> saveAll(Iterable<S> arg0) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public <S extends EmployeeEntity> S save(S arg0) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Optional<EmployeeEntity> findById(Integer arg0) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Iterable<EmployeeEntity> findAllById(Iterable<Integer> arg0) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Iterable<EmployeeEntity> findAll() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public boolean existsById(Integer arg0) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-			
-			@Override
-			public void deleteById(Integer arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void deleteAll(Iterable<? extends EmployeeEntity> arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void deleteAll() {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void delete(EmployeeEntity arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public long count() {
-				// TODO Auto-generated method stub
-				return 0;
-			}
-		};
+		return new RestTemplate();
 	}
 
 }
---
+++ Vaadindemo/src/main/java/springboot/spring/ui/MyUI.java
@@ -0,0 +1,111 @@
+package springboot.spring.ui;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.annotation.WebServlet;
+import javax.transaction.Transactional.TxType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.icons.VaadinIcons;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.shared.ui.ValueChangeMode;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.Grid;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
+
+import springboot.spring.entity.EmployeeEntity;
+
+
+/**
+ * This UI is the application entry point. A UI may either represent a browser window 
+ * (or tab) or some part of a html page where a Vaadin application is embedded.
+ * <p>
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * overridden to add component to the user interface and initialize non-component functionality.
+ */
+@Theme("mytheme")
+@SpringUI
+public class MyUI extends UI {
+	private static final long serialVersionUID = 1L;
+
+
+	RestTemplate service=new RestTemplate();
+	
+	Grid<EmployeeEntity> grid=new Grid<>(EmployeeEntity.class);
+	TextField textfield=new TextField();
+	
+	
+	@Override 
+    protected void init(VaadinRequest vaadinRequest) {
+        final VerticalLayout layout = new VerticalLayout();
+        
+        textfield.setPlaceholder("Serch by id");
+        textfield.addValueChangeListener(e -> employeesearch(textfield.getValue()));
+        textfield.setValueChangeMode(ValueChangeMode.LAZY);
+        Button clearFilterTextBtn = new Button(VaadinIcons.CLOSE);
+        clearFilterTextBtn.setDescription("Clear the current filter");
+        clearFilterTextBtn.addClickListener(e -> textfield.clear());
+        CssLayout filtering = new CssLayout();
+        filtering.addComponents(textfield, clearFilterTextBtn);
+        filtering.setStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
+        
+        grid.setColumns("employeeid","employeename");
+        grid.setItems(employeedata());
+        layout.addComponents(filtering, grid);
+        setContent(layout);
+    }
+	
+	private List<EmployeeEntity> employeedata(){
+		List<EmployeeEntity> ls=new ArrayList<EmployeeEntity>();
+		final String uri = "http://localhost:8087/employee";
+		ResponseEntity<List<EmployeeEntity>> employeeResponse =
+		        service.exchange(uri,
+		                    HttpMethod.GET, null, new ParameterizedTypeReference<List<EmployeeEntity>>() {
+		            });
+		ls=employeeResponse.getBody();
+		return ls;
+	}
+	
+	private List<EmployeeEntity> employeesearch(String id)
+	{
+		List<EmployeeEntity> ls=new ArrayList<EmployeeEntity>();
+		final String uri = "http://localhost:8087/employee/{id}";
+		Map<String,String> m=new HashMap<>();
+		m.put("id", id);
+		URI url = UriComponentsBuilder.fromUriString(uri)
+		        .buildAndExpand(m)
+		        .toUri();
+		ResponseEntity<EmployeeEntity> employeeResponse =
+		        service.exchange(url,
+		                    HttpMethod.GET, null,EmployeeEntity.class);
+		EmployeeEntity e= employeeResponse.getBody();
+		e.toString();
+		ls.add(e);
+		return ls;
+		
+	}
+
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = MyUI.class, productionMode = true)
+    public static class MyUIServlet extends VaadinServlet {
+    }
+}
